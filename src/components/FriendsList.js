3153fbe0013017fe1fd325b5c440fb3c472828c5
Islam Mohamed <islamelbannaa@gmail.com>
2019-01-29 04:13:06 +0200

    Friend list - Migirating with firebase

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -6,6 +6,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import PersonIcon from '@material-ui/icons/Person';
 
+import * as firebase from 'firebase';
+
 
 const styles = theme => ({
   menuItem: {
@@ -20,27 +22,67 @@ const styles = theme => ({
   icon: {},
 });
 
-class MessagesList extends React.Component {
-  
-  render() {
-    const { classes } = this.props;
-    // console.log(this.props);
-    return (
+class FriendList extends React.Component {
+
+    constructor(props) {
+        super(props);   
+        this.state = {
+            friends: Array(),
+        }
+
+        this.getFriends();
+    }
+
+    getFriends = () => {
         
-        <MenuList>
+        let user = firebase.auth().currentUser;
+        let db = firebase.firestore();
+        let _this = this;
 
-            {this.props.friends.map(friend => (
-                <MenuItem key={friend[0]} className={classes.menuItem}>
-                    <ListItemIcon className={classes.icon}>
-                        <PersonIcon />
-                    </ListItemIcon>
-                    <ListItemText classes={{ primary: classes.primary }} inset primary={friend[0]} />
-                </MenuItem>
-            ))}
-
+        db.doc(`users/${user.uid}`).get().then(userSnapshot => {
+            const friendsCount = userSnapshot.data().friends;
+            console.log(friendsCount);
+            if (friendsCount > this.state.friends.length) {
+                db.collection('friends')
+            .where('users', 'array-contains', user.uid)
+            .get()
+            .then(snapshot => {
+                snapshot.docs.forEach(doc => {
+                    const data = doc.data();
+                    let friend = data.users[0] !== user.uid ? data.users[0] : data.users[1];
+                    db.doc(`users/${friend}`)
+                        .get()
+                        .then(friendSnapshot => {
+                            const friendData = friendSnapshot.data();
+                            let friends = _this.state.friends;
+                            friends.push(friendData);
+                            this.setState({ friends });
+                        })
+                })
+            }).catch(error => console.error(error));
+            }
+        });
+    }
+  
+    render() {
+        const { classes } = this.props;
+        // console.log(this.props);
+        return (
             
-        </MenuList>
-    );
-  }
+            <MenuList>
+
+                {this.state.friends.map((friend, i) => (
+                    <MenuItem key={i} className={classes.menuItem}>
+                        <ListItemIcon className={classes.icon}>
+                            <PersonIcon />
+                        </ListItemIcon>
+                        <ListItemText classes={{ primary: classes.primary }} inset primary={friend.displayName} />
+                    </MenuItem>
+                ))}
+
+                
+            </MenuList>
+        );
+    }
 }
-export default withStyles(styles)(MessagesList);
+export default withStyles(styles)(FriendList);
